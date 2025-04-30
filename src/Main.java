782fb9e980168c1647aa156f13617f2d3949e541
Sergey <you@example.com>
2025-04-30 20:19:47 +0300

    V1.0

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 8665645;
+        int max  =  num % 10;
+        int min =  num % 10;
+       while (num != 0 ){
+
+           if (num %  10 >= max){
+               max =  num % 10;
+
+           }else if (num % 10 < min){
+               min =  num % 10;
+           }
+num = num / 10;
+
+
+       }
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
